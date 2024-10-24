a9a40f1658fd64ab3e28f5688aa2c844fdf99f23
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-10-24 14:12:20 +0000

    added checkout forms and templates

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-from products.models import Product
 
+from products.models import Product
 
 # Create your views here.
 
@@ -13,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -95,10 +96,8 @@ def remove_from_bag(request, item_id):
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-        
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
-
     
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -50,8 +50,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    
 
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
