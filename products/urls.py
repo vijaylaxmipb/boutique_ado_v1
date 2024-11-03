0747043c6c49439d890cf8f36349d463f48bd8d0
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-11-03 23:46:03 +0000

    Product admin - add product view,url and template

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-
-    
+    path('add/', views.add_product, name='add_product'),
 ]
